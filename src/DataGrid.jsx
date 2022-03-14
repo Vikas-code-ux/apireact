0ff39b65b5fad702d51ae821ded547e63efb9213
Vikas Varshney <vikashv20@iitk.ac.in>
2022-03-14 23:48:37 +0530

    change in width

--- src/DataGrid.jsx
+++ src/DataGrid.jsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'name', headerName: 'NAME', width: 130 },
-  { field: 'phone', headerName: 'NUMBER', width: 130 },
+  { field: 'name', headerName: 'NAME', width: 200 },
+  { field: 'phone', headerName: 'NUMBER', width: 200 },
   { field: 'email', headerName: 'EMAIL', width: 200 },
   { field: 'address.geo.lat', headerName: 'Latitude', width: 130 },
   { field: 'address.geo.lng', headerName: 'Longitude', width: 130 },
